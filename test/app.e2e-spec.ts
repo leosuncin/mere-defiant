94fb74407640b4125d645f59b2bdae244ba25497
Jaime Leonardo Suncin Cruz <leosuncin@users.noreply.github.com>
2023-06-24 01:35:11 +0000

    test(e2e): rewrite app test with pactum

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,28 +1,11 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { HttpStatus } from '@nestjs/common'
+import { spec } from 'pactum'
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
-
-  afterEach(async () => {
-    await app.close();
-  });
-
   it('/ (GET)', async () => {
-    await request(app.getHttpServer())
+    await spec()
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .expectStatus(HttpStatus.OK)
+      .expectBody('Hello World!');
   });
 });
